567e0d16865c8e46f55f397d252a0c0b154b7a42
Jacques <smutsaj@gmail.com>
2017-04-19 21:22:14 +0200

    Added ToDo's for next session

--- app/src/main/java/com/jacquessmuts/popularmovies/Activities/HomeActivity.java
+++ app/src/main/java/com/jacquessmuts/popularmovies/Activities/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.jacquessmuts.popularmovies.Activities;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,13 +8,23 @@ import android.util.Log;
 import com.jacquessmuts.popularmovies.R;
 import com.jacquessmuts.popularmovies.Utils.Server;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Server.getPopularMovies(new PopularMoviesListener());
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView(){
+        //TODO do that whole recyclerview setup thing throughout
+    }
+
+    @Override
+    public void onRefresh() {
+        //TODO: when the user refreshes through pull-to-refresh, or after data gets successfully loaded
     }
 
     public class PopularMoviesListener implements Server.ServerListener{
@@ -21,6 +32,7 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public void serverResponse(String response) {
             Log.d("Server Response", response);
+            //TODO Handle ServerResponse here. Use Gson.
         }
     }
 }
