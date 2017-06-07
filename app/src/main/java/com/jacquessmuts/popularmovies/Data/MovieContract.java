681c4d3bf3e39e41b3d9b1fff31f09b02378fdfa
Jacques Smuts <jsmuts@4imobile.co.za>
2017-06-07 15:34:25 +0200

    work in progress on Movie ContentProvider setup

---
+++ app/src/main/java/com/jacquessmuts/popularmovies/Data/MovieContract.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jacquessmuts.popularmovies.Data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import com.jacquessmuts.popularmovies.Models.Review;
+import com.jacquessmuts.popularmovies.Models.Trailer;
+
+import java.util.ArrayList;
+
+/**
+ * Defines table and column names for the weather database. This class is not necessary, but keeps
+ * the code organized.
+ */
+public class MovieContract {
+
+    /*
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website. A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * Play Store.
+     */
+    public static final String CONTENT_AUTHORITY = "com.jacquessmuts.popularmovies";
+
+    /*
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider for this app.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /*
+     * Possible paths that can be appended to BASE_CONTENT_URI to form valid URI's that the app
+     * can handle. For instance,
+     *
+     *     content://com.example.android.sunshine/weather/
+     *     [           BASE_CONTENT_URI         ][ PATH_MOVIE ]
+     *
+     * is a valid path for looking at weather data.
+     *
+     *      content://com.example.android.sunshine/givemeroot/
+     *
+     * will fail, as the ContentProvider hasn't been given any information on what to do with
+     * "givemeroot". At least, let's hope not. Don't be that dev, reader. Don't be that dev.
+     */
+    public static final String PATH_MOVIE = "movie";
+
+    /* Inner class that defines the table contents of the weather table */
+    public static final class MovieEntry implements BaseColumns {
+
+        /* The base CONTENT_URI used to query the Weather table from the content provider */
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_MOVIE)
+                .build();
+
+        /* Used internally as the name of our movies table. */
+        public static final String TABLE_NAME = "movies";
+
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_ORIGINAL_LANGUAGE = "original_language";
+        public static final String COLUMN_ORIGINAL_TITLE = "original_title";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+        public static final String COLUMN_BACKDROP_PATH = "backdrop_path";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_POPULARITY = "popularity";
+        public static final String COLUMN_VOTE_AVERAGE = "vote_average";
+        public static final String COLUMN_ADULT = "adult";
+        public static final String COLUMN_VOTE_COUNT = "vote_count";
+        public static final String COLUMN_TRAILERS = "trailers";
+        public static final String COLUMN_REVIEWS = "reviews";
+        public static final String COLUMN_IS_FAVORITE = "isFavorite";
+
+    }
+}
